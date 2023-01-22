d2347b0e227e1d5ea658a79a3f5544f56e058deb
shima8823 <taiyaki1275@gmail.com>
2023-01-22 11:00:41 +0900

    [update] refecter

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# ディレクトリであれば真
 if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then 
 	mysql_install_db
-	mysqld -u mysql --bootstrap << EOF
+	mysqld --bootstrap << EOF
+		FLUSH PRIVILEGES;
+		ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 		CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
 		CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-		GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-		ALTER USER root@localhost IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-		FLUSH PRIVILEGES;
+		GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%';
 EOF
 fi
 
+# mysqldデーモンを起動し、クライアンアントからの接続を受け付け、データベースへのアクセスを提供
 mysqld_safe
